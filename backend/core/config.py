fc49e1a88ba1247bc758cd2aa6c3e4342177661c
Axlmd <axlmd@outlook.com>
2024-12-23 00:05:25 -0500

    010: Ajustes .env

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,11 +1,10 @@
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
 
 class Settings(BaseSettings):
-    database_url: str = "sqlite:///./apidb.db"
-    database_url: str = "mysql+mysqldb://root:2002@localhost:3306/stresstracker"
-
-    class Config:
-        env_file = ".env"
-        env_file_encoding = 'utf-8'
+    database_url: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
